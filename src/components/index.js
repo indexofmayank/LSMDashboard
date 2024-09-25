d51d64c8a38b03f37a9a919fe6e915961bcdbe3c
indexofmayank <maimayanmanu@gmail.com>
2024-09-25 17:05:19 +0530

    firstcomit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import ErrorBoundary from './ErrorBoundary';
+import SidebarWithHeader from './SidebarWithHeader';
+import MobileNav from './MobileNav';
+import NavItem from './NavItem';
+import SidebarContent from './SidebarContent';
+export {
+    ErrorBoundary,
+    SidebarWithHeader,
+    MobileNav,
+    NavItem,
+    SidebarContent
+}
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+    SidebarWithHeader
+} from '../components';
+
+function Dashboard() {
+    return (
+        <SidebarWithHeader>
+            mayank
+        </SidebarWithHeader>
+    );
+}
+
+export default Dashboard;
